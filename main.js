bf4d3403bcaff3f77179db369ead110afd305aba
awannoman <160035058+awannoman@users.noreply.github.com>
2024-03-23 00:46:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+const answers = await inquirer.prompt([
+    {
+        name: "userGuessedNumber",
+        type: "number",
+        message: "please guessed the number between 1-10: ",
+    },
+]);
+if (answers.userGuessedNumber === randomNumber) {
+    console.log("congratulations! you guseed right number");
+}
+else {
+    console.log("you gussed wrong number");
+}
